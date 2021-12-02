0d52a1fa7a42722acdccd124beb5ef7e1523b629
Aditya Bhonsle <aditya.bhonsle@lntinfotech.com>
2021-12-02 15:57:46 +0530

    Event Basics

--- src/index.js
+++ src/index.js
@@ -51,20 +51,51 @@ function Nested() {
 const Person = () => <h2>John Doe</h2>
 const Message = () => <p>this is my message</p>
 
-const firstBook = {
-  img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
-  title:'Will',
-  author: 'Will Smith'
-}
-const secondBook = {
-  img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
-  title:'Will 2',
-  author: 'Will 2 Smith'
-}
+const books = [
+  {
+    id:1,
+    img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
+    title:'Will',
+    author: 'Will Smith'
+  }, 
+  {
+    id:2,
+    img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
+    title:'Will 2',
+    author: 'Will 2 Smith'
+  },
+  {
+    id:3,
+    img:"https://images-na.ssl-images-amazon.com/images/I/51znoqd9roL._AC_SX184_.jpg",
+    title:'Diary of Wimpy Kid',
+    author: 'Jeff Kinney'
+  }
+]
+// Simple List Iteration
+// const names = ['john','peter','susan']
+// const newNames = names.map((name)=>{
+//   console.log(name);
+//   return <h1>{name}</h1>;
+// })
+// console.log(newNames);
+
 function BookList(){
   return (
     <section className='booklist'>
-      <Book 
+      {books.map((book)=>{
+        //console.log(book);
+        //const {img,title,author} = book;
+        // return (
+        //   <div>
+        //     <h3>{title}</h3>
+        //     <h6>{author}</h6>
+        //   </div>
+        // )
+        //return <Book key={book.id} book={book}/>
+        //spread operator
+        return <Book key={book.id} {...book}/>
+      })}
+      {/* <Book 
         img={firstBook.img} 
         title={firstBook.title} 
         author={firstBook.author}
@@ -76,7 +107,7 @@ function BookList(){
       <Book 
         img={secondBook.img} 
         title={secondBook.title} 
-        author={secondBook.author}/>
+        author={secondBook.author}/> */}
     </section>
   )
 }
@@ -85,24 +116,43 @@ function BookList(){
 
 //const Book = ({img,title,author,children}) =>{
 const Book = (props) =>{
+  //console.log(props);
+  //const {img,title,author,children} = props.book;
+  //spread operator
   const {img,title,author,children} = props;
+  //atribute, eventhandler 
+  //onClick, onMouseOver
+  const clickHandler  = (e) =>{
+    console.log(e);
+    console.log(e.target);
+    alert('hello there');
+  };
+  const complex = (author) =>{
+    console.log(author);
+  }
   return (
-  <article>
+  <article className='book' onMouseOver={()=>{
+    console.log(author);
+  }}>
     <Image img={img}/>
     <Title title={title}/>
     <Author author={author}/>
-    {children}
+    {/* {children} */}
+    <button type='button' onClick={clickHandler}>
+      button
+    </button>
+    <button type='button' onClick={()=>complex(author)}>more complex example</button>
   </article>)
 };
-const Image = (props) => {
-  return (<img src={props.img} alt="" />)
+const Image = ({img}) => {
+  return (<img src={img} alt="" />)
 }
-const Title = (props) => {
-  return (<h1>{props.title}</h1>)
+const Title = ({title}) => {
+  return (<h1 onClick={()=>{alert(title)}}>{title}</h1>)
 } 
 // CSS JSX
-const Author = (props) => {
-  return (<h4>{props.author}</h4>)
+const Author = ({author}) => {
+  return (<h4>{author}</h4>)
 }
 
 // Props children
