1e0e1edf4d51c95534eaa3b791bd7b1a001a9ce5
Aditya Bhonsle <aditya.bhonsle@lntinfotech.com>
2021-12-02 15:04:56 +0530

    Props Basics

--- src/index.js
+++ src/index.js
@@ -51,33 +51,60 @@ function Nested() {
 const Person = () => <h2>John Doe</h2>
 const Message = () => <p>this is my message</p>
 
+const firstBook = {
+  img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
+  title:'Will',
+  author: 'Will Smith'
+}
+const secondBook = {
+  img:"https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg",
+  title:'Will 2',
+  author: 'Will 2 Smith'
+}
 function BookList(){
   return (
     <section className='booklist'>
-      <Book/>
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}
+      >
+        <p>
+          Ipsum Lorem dolor sit amet consecteuru  
+        </p>    
+      </Book>
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author}/>
     </section>
   )
 }
 
-const Book = () =>{
+
+
+//const Book = ({img,title,author,children}) =>{
+const Book = (props) =>{
+  const {img,title,author,children} = props;
   return (
   <article>
-    <Image/>
-    <Title/>
-    <Author/>
+    <Image img={img}/>
+    <Title title={title}/>
+    <Author author={author}/>
+    {children}
   </article>)
 };
-const Image = () => {
-  return (<img src="https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL127_SR127,127_.jpg" alt="" />)
+const Image = (props) => {
+  return (<img src={props.img} alt="" />)
 }
-const Title = () => {
-  const title = 'Will';
-  return (<h1>{title}</h1>)
+const Title = (props) => {
+  return (<h1>{props.title}</h1>)
 } 
 // CSS JSX
-const Author = () => {
-  const author = 'Will Smith';
-  return (<h4 style={{color:'red',fontSize:'0.75rem',matginTop:'0.25rem'}}>{author.toUpperCase()}</h4>)
+const Author = (props) => {
+  return (<h4>{props.author}</h4>)
 }
+
+// Props children
 
 ReactDom.render((<BookList/>),document.getElementById('root'));
